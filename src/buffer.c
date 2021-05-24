c8d20d7d4018cdaf6d48913a3ceb89608f34f1e2
topolyyy <parsa_alternate@protonmail.com>
2021-05-24 18:39:28 -0400

    added buf_extend and added const to parameters

--- src/buffer.c
+++ src/buffer.c
@@ -2,12 +2,18 @@
 
 #include <stdlib.h>
 
-void buf_append(buf_t* buf, u8 b) {
+void buf_append(buf_t* const buf, const u8 b) {
     ++buf->len;
     if (buf->len > buf->cap) {
         buf->cap *= 2;
         buf->p = realloc(buf->p, buf->cap);
     }
-    buf->p[buf->len-1] = b;
-    buf->p[buf->len] = 0;
+    buf->p[buf->len-2] = b;
+    buf->p[buf->len-1] = 0;
+}
+
+void buf_extend(buf_t *const buf, const u8 * const src, const u32 n) {
+    for (u32 i = 0; i < n; ++i) {
+        buf_append(buf, src[i]);
+    }
 }
