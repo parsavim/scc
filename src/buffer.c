7bd9404cd6d83c81a6d0b9f7bdc5ff6e6fde417b
topolyyy <parsa_alternate@protonmail.com>
2021-05-28 18:42:54 -0400

    remove buf_extend, add it inline to header file

--- src/buffer.c
+++ src/buffer.c
@@ -12,9 +12,3 @@ void buf_append(buf_t* const buf, char const b) {
     buf->p[buf->len-2] = b;
     buf->p[buf->len-1] = 0;
 }
-
-void buf_extend(buf_t *const buf, char const* const src, uint32_t const n) {
-    for (uint32_t i = 0; i < n; ++i) {
-        buf_append(buf, src[i]);
-    }
-}
