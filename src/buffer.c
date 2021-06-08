c8e9a37d5e5eee65ba57207b8b20bc970dbd3dab
topolyyy <parsa_alternate@protonmail.com>
2021-06-08 18:22:53 -0400

    change buf_append to check if buf->cap is 0

--- src/buffer.c
+++ src/buffer.c
@@ -6,8 +6,13 @@
 void buf_append(buf_t* const buf, char const b) {
     ++buf->len;
     if (buf->len > buf->cap) {
-        buf->cap *= 2;
-        buf->p = realloc(buf->p, buf->cap);
+        if (buf->cap == 0) {
+            buf->cap = ++buf->len;
+            buf->p = malloc(buf->p, buf->cap);
+        } else {
+            buf->cap *= 2;
+            buf->p = realloc(buf->p, buf->cap);
+        }
     }
     buf->p[buf->len-2] = b;
     buf->p[buf->len-1] = 0;
